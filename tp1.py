28923882d06e7c0a63bdf2f1ef0854a7eda4f98c
DeclerusAllen <declerusallen20@gmail.com>
2024-05-14 13:46:12 -0400

    the first

---
+++ tp1.py
@@ -0,0 +1,11 @@
+non="Allen declerus Jean"
+#an miniskil
+#chèn de karaktè an lis
+miniskil=non.lower()
+print(miniskil)
+name=miniskil.split()
+#premye lèt de chak non
+print(name)
+tit=non.title()
+print(tit)
+#
